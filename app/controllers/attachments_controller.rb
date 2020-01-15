2df7d3fefb672a93be2f8e4431c35d9b02269a8e
kilya <ikondratev@wirecapital.com>
2020-01-16 00:07:13 +0300

    fps_5_active_storage/Fix: attachment: author check

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,9 @@
 class AttachmentsController < ApplicationController
   def destroy
-    @file = ActiveStorage::Attachment.find(params[:id])
-    @file.purge
-    redirect_back(fallback_location: question_path)
+    file = ActiveStorage::Attachment.find(params[:id])
+    if current_user == file.record.user
+      file.purge
+      redirect_back(fallback_location: question_path)
+    end
   end
 end
