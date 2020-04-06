1eab10612b2fc4a8d75d10197c35b1fa2410bce5
kilya <ikondratev@wirecapital.com>
2020-04-07 01:15:15 +0300

    fps_9_auth: Fix binding

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -19,7 +19,6 @@ class AuthorizationsController < ApplicationController
       @user.save
 
       AuthMailer.notify_record(email: @email, token: @user.confirmation_token).deliver_now
-      binding.pry
 
       render 'authorizations/confirmation'
     end
