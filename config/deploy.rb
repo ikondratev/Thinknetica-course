8c228464da0a8c264f04749035bbb71e0988ea30
kilya <ikondratev@wirecapital.com>
2020-05-03 19:51:49 +0300

    master: add Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.13.0"
+
+set :application, "fps"
+set :repo_url, "git@github.com:ikondratev/fps.git"
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/fps"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,23 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server "163.172.174.31", user: "deployer", roles: %w{app db web}, primary: true
+set :rails_env, :production
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+ set :ssh_options, {
+   keys: %w(/home/vedro/.ssh/id_rsa),
+   forward_agent: true,
+   auth_methods: %w(publickey password), 
+   port: 2222
+ }
