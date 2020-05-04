801b98b80f66e370ab0c3eba4f1f244bf30c9c6e
kilya <ikondratev@wirecapital.com>
2020-05-04 20:04:49 +0300

    master: revert binding.pry

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,6 @@ class SearchesController < ApplicationController
   skip_authorization_check
 
   def index
-  	binding.pry
     @results = SearchService.search_by(params[:query], params[:type])
   end
 end
